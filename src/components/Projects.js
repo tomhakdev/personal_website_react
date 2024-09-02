3d59e3a4a553fc42b90f8c992cba638ee973decb
tomhakdev <134178320+tomhakdev@users.noreply.github.com>
2024-09-01 21:50:01 -0400

    Change project layout

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -10,114 +10,126 @@ const Projects = () => {
         
 
       <div className="project-item">
+        <div className="thumbnail">
+          <a href="https://magenta-cendol-cd03f3.netlify.app/" target="_blank" rel="noopener noreferrer">
+            <img src="/images/CSDD_Website.png" alt="CSDD Website"/>
+          </a>
+        </div>
+        <div className="project-content">
           <h3 className="project-title">Web Map Interface</h3>
-          <div className="thumbnail">
-            <a href="https://magenta-cendol-cd03f3.netlify.app/" target="_blank" rel="noopener noreferrer">
-              <img src="/images/CSDD_Website.png" alt="CSDD Website"/>
-            </a>
-          </div>
-          <div className="project-description">
-            <p>Developing a website for the Laboratory on Ethnic Conflict Research at the Centre for the Study of Democracy and Diversity. The site features an interactive map interface highlighting countries involved in territorial conflicts since WWII.</p>
-            <p>Skills: JavaScript, HTML, CSS</p>
-          </div>
+          <h4 className="project-subtitle">Laboratory on Ethnic Conflict Research</h4>
+          <p className="project-description">Developing a website for the Laboratory on Ethnic Conflict Research at the Centre for the Study of Democracy and Diversity. The site features an interactive map interface highlighting countries involved in territorial conflicts since WWII.</p>
+          <p className="project-skills">Skills: JavaScript, HTML, CSS</p>
+          <a href="https://magenta-cendol-cd03f3.netlify.app/" className="learn-more-btn">Learn More</a>
         </div>
+      </div>
 
-
-        <div className="project-item">
+      <div className="project-item">
+        <div className="thumbnail">
+          <a href="https://github.com/SoslyLW/essdev-app-team" target="_blank" rel="noopener noreferrer">
+            <img src="/images/ToolPool.png" alt="ToolPool Project"/>
+          </a>
+        </div>
+        <div className="project-content">
           <h3 className="project-title">ToolPool</h3>
-          <div className="thumbnail">
-            <a href="https://github.com/SoslyLW/essdev-app-team" target="_blank" rel="noopener noreferrer">
-              <img src="/images/ToolPool.png" alt="ToolPool Project"/>
-            </a>
-          </div>
-          <div className="project-description">
-            <p>Tool-sharing app (IOS + Android) created as part of the Queen's Engineering Society Software Development Team (ESSDEV). The app enables users to create profiles, connect with others in their community, and borrow tools, fostering a shared resource pool.</p>
-            <p>Skills: Flutter, Firebase, Android Studio</p>
-          </div>
+          <h4 className="project-subtitle">ESSDEV</h4>
+          <p className="project-description">Tool-sharing app (IOS + Android) created as part of the Queen's Engineering Society Software Development Team (ESSDEV). The app enables users to create profiles, connect with others in their community, and borrow tools, fostering a shared resource pool.</p>
+          <p className="project-skills">Skills: Flutter, Firebase, Android Studio</p>
+          <a href="https://github.com/SoslyLW/essdev-app-team" className="learn-more-btn">Learn More</a>
         </div>
+      </div>
 
-
-        <div className="project-item">
+      <div className="project-item">
+        <div className="thumbnail">
+          <a href="https://github.com/SimonNRisk/speech-app" target="_blank" rel="noopener noreferrer">
+            <img src="/images/STAM_Logo.png" alt="STAM.ai Project"/>
+          </a>
+        </div>
+        <div className="project-content">
           <h3 className="project-title">STAM.ai</h3>
-          <div className="thumbnail">
-            <a href="https://github.com/SimonNRisk/speech-app" target="_blank" rel="noopener noreferrer">
-              <img src="/images/STAM_Logo.png" alt="ToolPool Project"/>
-            </a>
-          </div>
-          <div className="project-description">
-            <p>AI speech-thearpy startup I am co-founding. In the process of developing a web app that helps users improve speech impediments through engaging, gamified exercises. The app leverages AI to accurately assess whether words are pronounced correctly, providing real-time feedback for users. Website coming soon...</p>
-            <p>Skills: n/a</p>
-          </div>
+          <h4 className="project-subtitle">inQUbate</h4>
+          <p className="project-description">AI speech-therapy startup I am co-founding. In the process of developing a web app that helps users improve speech impediments through engaging, gamified exercises. The app leverages AI to accurately assess whether words are pronounced correctly, providing real-time feedback for users. Website coming soon...</p>
+          <p className="project-skills">Skills: n/a</p>
+          <a href="https://github.com/SimonNRisk/speech-app" className="learn-more-btn">Learn More</a>
         </div>
+      </div>
 
-
-        <div className="project-item">
+      <div className="project-item">
+        <div className="thumbnail">
+          <a href="https://github.com/tomhakdev/Pest-Control-System" target="_blank" rel="noopener noreferrer">
+            <img src="/images/Home Security System.jpg" alt="Pest Security System"/>
+          </a>
+        </div>
+        <div className="project-content">
           <h3 className="project-title">Pest Security System</h3>
-          <div className="thumbnail">
-            <a href="https://github.com/tomhakdev/Pest-Control-System" target="_blank" rel="noopener noreferrer">
-              <img src="/images/Home Security System.jpg" alt="Pest Security System"/>
-            </a>
-          </div>
-          <div className="project-description">
-            <p>Pest detection device built as part of the ECE Design and Practice course. This ground-based device uses sound waves to detect nearby motion, triggering flashing lights and a loud noise to scare away pests. It’s a practical solution for protecting crops and gardens from unwanted intruders.</p>
-            <p>Skills: Arduino, C++, HTML, CSS</p>
-          </div>
+          <h4 className="project-subtitle">ELEC 290</h4>
+          <p className="project-description">Pest detection device built as part of the ECE Design and Practice course. This ground-based device uses sound waves to detect nearby motion, triggering flashing lights and a loud noise to scare away pests. It’s a practical solution for protecting crops and gardens from unwanted intruders.</p>
+          <p className="project-skills">Skills: Arduino, C++, HTML, CSS</p>
+          <a href="https://github.com/tomhakdev/Pest-Control-System" className="learn-more-btn">Learn More</a>
         </div>
+      </div>
 
-
-        <div className="project-item">
+      <div className="project-item">
+        <div className="thumbnail">
+          <a href="https://github.com/tomhakdev/Movement-Detection-Desktop-App" target="_blank" rel="noopener noreferrer">
+            <img src="/images/MovementDetection.png" alt="Movement Detection System"/>
+          </a>
+        </div>
+        <div className="project-content">
           <h3 className="project-title">Movement Detection</h3>
-          <div className="thumbnail">
-            <a href="https://github.com/tomhakdev/Movement-Detection-Desktop-App" target="_blank" rel="noopener noreferrer">
-              <img src="/images/MovementDetection.png" alt="Pest Security System"/>
-            </a>
-          </div>
-          <div className="project-description">
-            <p>Desktop app for distinguishing between walking and jumping, using data collected via the Phyphox app. Created as part of a Data Science course, the app processes sensor data to accurately identify and categorize these movements, showcasing the application of data analysis in real-time motion detection.</p>
-            <p>Skills: Python, Pandas, NumPy, Tkinter, Phyphox</p>
-          </div>
+          <h4 className="project-subtitle">ELEC 292</h4>
+          <p className="project-description">Desktop app for distinguishing between walking and jumping, using data collected via the Phyphox app. Created as part of a Data Science course, the app processes sensor data to accurately identify and categorize these movements, showcasing the application of data analysis in real-time motion detection.</p>
+          <p className="project-skills">Skills: Python, Pandas, NumPy, Tkinter, Phyphox</p>
+          <a href="https://github.com/tomhakdev/Movement-Detection-Desktop-App" className="learn-more-btn">Learn More</a>
         </div>
+      </div>
 
-
-        <div className="project-item">
+      <div className="project-item">
+        <div className="thumbnail">
+          <a href="https://github.com/tomhakdev/GuessMasterV3" target="_blank" rel="noopener noreferrer">
+            <img src="/images/GuessMaster.png" alt="GuessMaster app"/>
+          </a>
+        </div>
+        <div className="project-content">
           <h3 className="project-title">Guess Master</h3>
-          <div className="thumbnail">
-            <a href="https://github.com/tomhakdev/GuessMasterV3" target="_blank" rel="noopener noreferrer">
-              <img src="/images/GuessMaster.png" alt="GuessMaster app"/>
-            </a>
-          </div>
-          <div className="project-description">
-            <p>Guessing game app using Java and Android Studio as part of the Object-Oriented Programming course. The app challenges users to guess the birth date of a celebrity or the founding date of a country based on an image. It features a ticket reward system that rewards players for correct answers, enhancing engagement and learning.</p>
-            <p>Skills: Java, Android Studio</p>
-          </div>
+          <h4 className="project-subtitle">ELEC 279</h4>
+          <p className="project-description">Guessing game app using Java and Android Studio as part of the Object-Oriented Programming course. The app challenges users to guess the birth date of a celebrity or the founding date of a country based on an image. It features a ticket reward system that rewards players for correct answers, enhancing engagement and learning.</p>
+          <p className="project-skills">Skills: Java, Android Studio</p>
+          <a href="https://github.com/tomhakdev/GuessMasterV3" className="learn-more-btn">Learn More</a>
         </div>
+      </div>
 
-
-        <div className="project-item">
+      <div className="project-item">
+        <div className="thumbnail">
+          <a href="https://github.com/tomhakdev/CLISpreadsheet" target="_blank" rel="noopener noreferrer">
+            <img src="/images/Spreadsheet.png" alt="Spreadsheet Application"/>
+          </a>
+        </div>
+        <div className="project-content">
           <h3 className="project-title">Spreadsheet Application</h3>
-          <div className="thumbnail">
-            <a href="https://github.com/tomhakdev/CLISpreadsheet" target="_blank" rel="noopener noreferrer">
-              <img src="/images/Spreadsheet.png" alt="Spreadsheet"/>
-            </a>
-          </div>
-          <div className="project-description">
-            <p>Excel clone entirely in C, replicating the core functionality of a basic spreadsheet app. Users can input data and perform various operations on it, simulating real-world spreadsheet tasks. This project was completed as part of the Data Structures and Algorithms course.</p>
-            <p>Skills: C</p>
-          </div>
+          <h4 className="project-subtitle">ELEC 278</h4>
+          <p className="project-description">Excel clone entirely in C, replicating the core functionality of a basic spreadsheet app. Users can input data and perform various operations on it, simulating real-world spreadsheet tasks. This project was completed as part of the Data Structures and Algorithms course.</p>
+          <p className="project-skills">Skills: C</p>
+          <a href="https://github.com/tomhakdev/CLISpreadsheet" className="learn-more-btn">Learn More</a>
         </div>
+      </div>
+
+
 
         <div className="project-item">
-          <h3 className="project-title">Personal Website</h3>
           <div className="thumbnail">
             <a href="https://github.com/tomhakdev/Personal-Website" target="_blank" rel="noopener noreferrer">
               <img src="/images/web-homepage.png" alt="Website Home"/>
             </a>
           </div>
-          <div className="project-description">
-            <p>The code behind the creation of this website</p>
-            <p>Skills: React.js, HTML, CSS</p>
+          <div className="project-content">
+            <h3 className="project-title">Personal Website</h3>
+            <p className="project-description">The code behind the creation of this website</p>
+            <p className="project-skills">Skills: React.js, HTML, CSS</p>
+            <a href="https://github.com/tomhakdev/personal-website-react" className="learn-more-btn">Learn More</a>
           </div>
         </div>
+
 
       </div>
     </section>
